fbec092e932ade8a94d8a1bd979036f991ac4a9a
Messy <729578197@qq.com>
2019-06-14 00:35:07 +0800

    第一版完成

---
+++ components/nx-cell.vue
@@ -0,0 +1,62 @@
+<template>
+	<view class="nx-cell hCenter" hover-class="nx-cell-active">
+		<view class="nx-cell-left">
+			<image class="nx-cell-icon" :src="icon"></image>
+		</view>
+		<view class="nx-cell-right hCenter" :class="border?'border-b':''">
+			<text style="width: 90%">{{title}}</text>
+			<image class="nx-cell-right2" src="../static/next.png"></image>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {};
+		},
+		props: {
+			icon: String,
+			title: String,
+			border: {
+				type: Boolean,
+				value: true
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	.nx-cell {
+		background: white;
+		color: #353535;
+	}
+
+	.nx-cell-left {
+		padding: 31upx 36upx;
+	}
+
+	.nx-cell-right {
+		width: 100%;
+		font-size: 36upx;
+		padding: 31upx 0;
+	}
+
+	.nx-cell-icon {
+		width: 40upx;
+		height: 40upx;
+	}
+
+	.nx-cell-active {
+		background-color: #e5e5e5;
+	}
+
+	.nx-cell-right2 {
+		width: 60upx;
+		height: 60upx;
+	}
+
+	.border-b {
+		border-bottom: 1upx solid #F3F3F3;
+	}
+</style>
--- pages/more/more.vue
+++ pages/more/more.vue
@@ -1,29 +1,60 @@
 <template>
-	<view class="whCenter">
-		<view class="whCenter" style="flex-direction: column;background-color: #FFFFFF;width: 94%;">
-			<text>{{title}}</text>
-			<text>{{name}}</text>
+	<view>
+		<guide v-if="showGuide">awerfawe</guide>
+		<button open-type="share">
+			<nx-cell icon="/static/share.png" border title="分享给好友"></nx-cell>
+		</button>
+		<button open-type="contact">
+			<nx-cell title="我要吐槽" icon="/static/service.png"></nx-cell>
+		</button>
+		<view class="hCenter" style="color: #957B30;flex-direction: column;font-size: 26upx;position: fixed;bottom: 10upx;left: 20%;">
+			<!-- <text>『公式大全2』是一款查询各类公式的神器，现处于项目创建阶段，功能会逐步完善。</text>
+			<text>您的支持和反馈是我们最大的动力，喜欢可以加入我的小程序。</text> -->
+			<text>©liuli作品 V1.0.0</text>
+			<text>本程序所有资料均来自互联网，仅供参考</text>
 		</view>
 	</view>
 </template>
 
 <script>
+	import nxCell from '../../components/nx-cell.vue';
+	import guide from '../../components/guide.vue';
+	
 	export default {
 		data() {
-			return {
-				title:'Hello',
-				name:'更多'
-			}
-		},
-		onLoad() {
-	
+			return {}
 		},
 		methods: {
-			
+			tipAttention() {
+				this.showGuide = true;
+				setTimeout(() => {
+					this.showGuide = false;
+				}, 4000)
+			},
+			onShareAppMessage() {
+				return {
+					title: '公式大全2',
+					path: '/pages/index/index'
+				}
+			},
+		},
+		components: {
+			nxCell,
+			guide
 		}
 	}
 </script>
 
 <style>
-	
+	button {
+		padding: 0;
+		line-height: 1.6;
+		font-size: initial;
+		text-align: initial;
+		border-radius: inherit;
+	}
+	button:after {
+		border: none;
+		border-radius: 0;
+	}
 </style>
